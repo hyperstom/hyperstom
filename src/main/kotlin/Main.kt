4e8081ff7ea515829bf80b5c1930d7b7fdef5caf
infernostars <blazem7708.2@gmail.com>
2023-08-24 10:47:00 -0600

    stop being the intellij default project Now!!

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,34 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+package emeraldwater.infernity.dev
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+import net.minestom.server.MinecraftServer
+import net.minestom.server.coordinate.Pos
+import net.minestom.server.entity.Player
+import net.minestom.server.event.GlobalEventHandler
+import net.minestom.server.event.player.PlayerLoginEvent
+import net.minestom.server.extras.MojangAuth
+import net.minestom.server.instance.InstanceContainer
+import net.minestom.server.instance.InstanceManager
+import net.minestom.server.instance.block.Block
+
+object Main {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val server: MinecraftServer = MinecraftServer.init()
+        MojangAuth.init()
+        server.start("0.0.0.0", 25565)
+        val instanceManager: InstanceManager = MinecraftServer.getInstanceManager()
+        // Create the instance
+        val instanceContainer: InstanceContainer = instanceManager.createInstanceContainer()
+        // Set the ChunkGenerator
+        instanceContainer.setGenerator { unit ->
+            unit.modifier().fillHeight(0, 40, Block.GRASS_BLOCK)
+        }
+        // Add an event callback to specify the spawning instance (and the spawn position)
+        val globalEventHandler: GlobalEventHandler = MinecraftServer.getGlobalEventHandler()
+        globalEventHandler.addListener(PlayerLoginEvent::class.java) { event ->
+            val player: Player = event.getPlayer()
+            event.setSpawningInstance(instanceContainer)
+            player.setRespawnPoint(Pos(0.0, 42.0, 0.0))
+        }
+    }
 }
